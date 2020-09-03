4042434fa7f157b02f20b014958c63981a3e866e
Suada Haji <suadahaji@gmail.com>
2020-09-03 11:17:14 +0300

    chore: Display Map

---
+++ app/src/main/java/com/suadahaji/weatherapp/views/CityListFragment.java
@@ -0,0 +1,7 @@
+package com.suadahaji.weatherapp.views;
+
+import androidx.fragment.app.Fragment;
+
+public class CityListFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/suadahaji/weatherapp/views/MapFragment.java
@@ -0,0 +1,50 @@
+package com.suadahaji.weatherapp.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.suadahaji.weatherapp.R;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    private GoogleMap map;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null)
+            mapFragment.getMapAsync(this);
+        return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng city = new LatLng(39.3999, -8.2245);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                city).zoom(6).build();
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        map.setOnMapClickListener(this);
+        map.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        map.addMarker(new MarkerOptions().position(latLng));
+    }
+}
+
